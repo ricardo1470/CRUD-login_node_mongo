76b3a85b614844b17b30b2f3dd1edf0e7ab8085f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-06-14 16:35:47 -0500

    create files

---
+++ src/app.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const createError = require('http-errors');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const path = require('path');
+const methodOverrie = require('method-override');
+const session = require('express-session');
+
+const app = express();
+
+const port = 9000 || process.env.PORT;
+
+// importing routes
+const indexRoutes = require('./routes/index');
+
+//settings
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//midelwares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')))
+app.use(methodOverrie('_method'));
+app.use(session({
+    secret: 'mysecretapp',
+    resave: true,
+    saveUninitialized: true
+}));
+
+//global variables
+
+//routes
+app.use('/', indexRoutes);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    //set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error.html');
+});
+
+// listen server on port
+app.listen(port, function () {
+    console.log(`CORS-enabled, web server listening on port: ${port}`)
+});
